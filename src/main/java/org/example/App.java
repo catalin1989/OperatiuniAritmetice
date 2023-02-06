ca7ee99ca31218390493d44c1b457e3cd7e45f45
catalin1989 <catalin_chira05@yahoo.com>
2023-02-06 18:54:56 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Sum adunare = new Sum();
+
+        adunare.sum(2,5);
+
+
+
+
+    }
+}
---
+++ src/main/java/org/example/Sum.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Sum {
+
+    int x;
+    int y;
+
+    public void sum(int x, int y){
+        System.out.println("Suma dintre cele doua variabile este: "+(x+y));
+    }
+}
